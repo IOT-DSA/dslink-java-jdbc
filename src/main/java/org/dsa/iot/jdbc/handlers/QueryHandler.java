5e8b0ac4414cc400339442a51a50e563aa654e59
Peter Shvets <p.shvets@dglogik.com>
2015-05-27 10:56:08 -0700

    sdk version updated

--- src/main/java/org/dsa/iot/jdbc/handlers/QueryHandler.java
+++ src/main/java/org/dsa/iot/jdbc/handlers/QueryHandler.java
@@ -1,7 +1,10 @@
 package org.dsa.iot.jdbc.handlers;
 
+import org.dsa.iot.dslink.node.Node;
 import org.dsa.iot.dslink.node.actions.ActionResult;
+import org.dsa.iot.dslink.node.value.Value;
 import org.dsa.iot.jdbc.model.JdbcConfig;
+import org.dsa.iot.jdbc.model.JdbcConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vertx.java.core.Handler;
@@ -21,5 +24,16 @@ public class QueryHandler implements Handler<ActionResult> {
 	public void handle(ActionResult event) {
 		LOG.info("Entering query connection handle");
 
+		Value value = event.getParameter(JdbcConstants.SQL);
+
+		Node status = config.getNode().getChild(JdbcConstants.STATUS);
+		if (value != null && value.getString() != null
+				&& !value.getString().isEmpty()) {
+
+			String sql = value.getString();
+			LOG.info(sql.toString());
+		} else {
+			status.setValue(new Value("slq is empty"));
+		}
 	}
 }
