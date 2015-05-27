7307ab4ca73c160459c5576b14c215bf18e1631a
Peter Shvets <p.shvets@dglogik.com>
2015-05-27 12:39:59 -0700

    AccessController.doPrivileged added

--- src/main/java/org/dsa/iot/jdbc/driver/JdbcDriverLoader.java
+++ src/main/java/org/dsa/iot/jdbc/driver/JdbcDriverLoader.java
@@ -2,6 +2,8 @@ package org.dsa.iot.jdbc.driver;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,10 +16,18 @@ import java.sql.SQLException;
  *
  */
 public class JdbcDriverLoader {
-	public static void registerDriver(URL url, String className)
+	public static void registerDriver(final URL url, String className)
 			throws InstantiationException, IllegalAccessException,
 			ClassNotFoundException, SQLException {
-		URLClassLoader loader = new URLClassLoader(new URL[] { url });
+
+		URLClassLoader loader = AccessController
+				.doPrivileged(new PrivilegedAction<URLClassLoader>() {
+					@Override
+					public URLClassLoader run() {
+						return new URLClassLoader(new URL[] { url });
+					}
+				});
+
 		Driver driver = (Driver) Class.forName(className, true, loader)
 				.newInstance();
 		DriverManager.registerDriver(new JdbcDriverHolder(driver));
