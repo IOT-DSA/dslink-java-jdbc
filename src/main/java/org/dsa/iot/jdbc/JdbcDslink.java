a67bd1e9cc81f0380d23de8fe8ea907ed1250d91
samrg472 <samrg472@gmail.com>
2015-08-14 13:00:12 -0700

    Fix missing drivers in the runtime manager

--- src/main/java/org/dsa/iot/jdbc/JdbcDslink.java
+++ src/main/java/org/dsa/iot/jdbc/JdbcDslink.java
@@ -3,10 +3,17 @@ package org.dsa.iot.jdbc;
 import org.dsa.iot.dslink.DSLink;
 import org.dsa.iot.dslink.DSLinkFactory;
 import org.dsa.iot.dslink.DSLinkHandler;
-import org.dsa.iot.jdbc.model.JdbcConstants;
 import org.dsa.iot.jdbc.provider.JdbcProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Dslink to work with JDBC
@@ -21,6 +28,82 @@ public class JdbcDslink extends DSLinkHandler {
     @Override
 	public boolean isResponder() {
         return true;
+    }
+
+    @Override
+    public void preInit() {
+        try {
+            URLClassLoader loader;
+            {
+                ClassLoader l = JdbcDslink.class.getClassLoader();
+                loader = (URLClassLoader) l;
+            }
+            URL[] urls = loader.getURLs();
+            for (URL url : urls) {
+                processUrl(loader, url);
+            }
+        } catch (Exception e) {
+            String msg = "Error scanning for drivers. ";
+            msg += "Relying on Java's mechanism for driver ";
+            msg += "initialization.";
+            LOG.error(msg, e);
+        }
+    }
+
+    private void processUrl(ClassLoader loader, URL url) throws Exception {
+        final String driverName = Driver.class.getName();
+        try (ZipInputStream zis = new ZipInputStream(url.openStream())) {
+            while (true) {
+                ZipEntry entry = zis.getNextEntry();
+                if (entry == null) {
+                    break;
+                }
+                {
+                    String name = entry.getName();
+                    if (entry.isDirectory()
+                            || !name.endsWith(driverName)) {
+                        continue;
+                    }
+                }
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                int read;
+                byte[] buffer = new byte[1024];
+                while ((read = zis.read(buffer)) >= 0) {
+                    baos.write(buffer, 0, read);
+                }
+                if (baos.size() <= 0) {
+                    continue;
+                }
+                String[] data = baos.toString("UTF-8").split("\n");
+                if (data.length <= 0) {
+                    continue;
+                }
+
+                for (String clazzName : data) {
+                    registerDriver(loader, clazzName);
+                }
+            }
+        }
+    }
+
+    private void registerDriver(ClassLoader loader, String clazzName) {
+        try {
+            clazzName = clazzName.trim();
+            Class<?> clazz = loader.loadClass(clazzName);
+            if (Driver.class.isAssignableFrom(clazz)) {
+                Object instance = clazz.newInstance();
+                Driver d = (Driver) instance;
+                DriverManager.registerDriver(d);
+
+                String msg = "Registered driver: {}";
+                LOG.info(msg, clazz.getName());
+            }
+        } catch (NoClassDefFoundError ignored) {
+        } catch (Exception e) {
+            String msg = "Failed to register driver: {}";
+            LOG.warn(msg, clazzName, e);
+        }
     }
 
     @Override
